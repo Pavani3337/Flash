effd3b4d11772a918d264fd22aa86e0e65e40604
pavani2007 <pavanichandrapatla@gmail.com>
2026-05-18 20:43:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let cards = [];
+let cards = JSON.parse(localStorage.getItem("flashcards")) || [];
 let index = 0;
 let flipped = false;
+
+// SAVE TO STORAGE
+function saveCards() {
+    localStorage.setItem("flashcards", JSON.stringify(cards));
+}
 
 // ADD CARD
 function addCard() {
@@ -8,9 +13,9 @@ function addCard() {
     let q = document.getElementById("question").value;
     let a = document.getElementById("answer").value;
 
-    if(q === "" || a === "") return;
+    if (q === "" || a === "") return;
 
-    cards.push({q, a});
+    cards.push({ q, a });
 
     document.getElementById("question").value = "";
     document.getElementById("answer").value = "";
@@ -18,13 +23,17 @@ function addCard() {
     index = cards.length - 1;
     flipped = false;
 
+    saveCards();
     showCard();
 }
 
 // SHOW CARD
 function showCard() {
 
-    if(cards.length === 0) return;
+    if (cards.length === 0) {
+        document.getElementById("card").innerText = "No flashcards yet";
+        return;
+    }
 
     let card = cards[index];
 
@@ -32,30 +41,36 @@ function showCard() {
         flipped ? card.a : card.q;
 }
 
-// FLIP CARD
+// FLIP
 function flipCard() {
-    if(cards.length === 0) return;
+    if (cards.length === 0) return;
 
     flipped = !flipped;
     showCard();
 }
 
-// NEXT CARD
+// NEXT
 function nextCard() {
-
-    if(index < cards.length - 1) {
+    if (index < cards.length - 1) {
         index++;
         flipped = false;
         showCard();
     }
 }
 
-// PREVIOUS CARD
+// PREV
 function prevCard() {
-
-    if(index > 0) {
+    if (index > 0) {
         index--;
         flipped = false;
         showCard();
     }
 }
+
+// AUTO LOAD WHEN PAGE OPENS
+window.onload = function () {
+    if (cards.length > 0) {
+        index = 0;
+        showCard();
+    }
+};
